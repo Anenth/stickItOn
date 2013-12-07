7baa5e4b10af07b9531648ce553f217c7ac1c07c
Anenth <anenthvishnu@gmail.com>
2013-12-07 17:15:39 +0530

    Tunning with server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,29 @@
 'use strict';
+/*global $ */
 
 angular.module('StickItOnApp')
 .controller('MainCtrl', function ($scope, Tabservice) {
-  Tabservice.getTabs();
-  $scope.addNew = function(){
-    $scope.tagAddNew = true;
-  };
-  $scope.newTab = function(){
-    Tabservice.newTab({
-      title : $scope.inputTitle,
-      hashtags : $scope.inputTags
+
+  /* get all the tabs data*/
+  function getAllTabs(){
+    Tabservice.getTabs().then(function(response){
+      $scope.Tabs = response.data;
     });
   }
+
+  getAllTabs();
+
+  $scope.newTab = function(){
+    var data = {
+      title : $scope.inputTitle,
+      hashtags : $scope.inputTags
+    };
+    Tabservice.newTab($.param(data)).then(function(){
+      $scope.Tabs.push(data);
+    });
+  };
+  $scope.delete = function(index,hashtags){
+    $scope.Tabs.splice(index, 1);
+    Tabservice.deleteTab(hashtags);
+  };
 });
--- app/scripts/services/tabService.js
+++ app/scripts/services/tabService.js
@@ -2,38 +2,45 @@
 
 angular.module('StickItOnApp')
 .service('Tabservice', function($http, Flashservice) {
-  var authEmail = 'anenthvishnu@gmail.com';
+
   var url = 'http://gcdc2013-keeptabson.appspot.com/tag/';
   var sendRequestToServer = function(type, url, data, alert){
-    $http({
+
+    return $http({
       method: type,
       url:url,
       data: data,
       withCredentials: true,
       cache: false,
       crossDomain: true,
-      headers : { "x-requested-with" : "XMLHttpRequest" }
-    }).success(function(data, status){
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).then(function(response){
       if(alert){
         Flashservice.show(' message ', 'success');
       }
-      if(data){
-        return data;
+      if(response.data){
+        console.log('Data received');
       }
-    }).error(function(data){
+      return response;
+    }, function(response) {
+      console.log('Data not received');
       if(alert){
-        for(var i in data){
+        for(var i in response.data){
           Flashservice.show(data[i], 'danger');
         }
       }
+      return response;
     });
   };
-  return{
+  return {
     newTab: function(data){
-      sendRequestToServer('POST', url, data, true);
+      return sendRequestToServer('POST', url, data, true);
     },
     getTabs: function(){
-      sendRequestToServer('GET', url);
+      return sendRequestToServer('GET', url);
+    },
+    deleteTab: function(data){
+      return sendRequestToServer('DELETE', url+encodeURIComponent(data));
     }
   };
 });

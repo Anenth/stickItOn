f03cfb1a89909bfe76646c4edaa9b0865ea16f94
Anenth Vishnu <anenth@Anenths-MacBook-Pro.local>
2014-01-19 20:47:42 +0530

    Youtube video regx fix

--- app/scripts/directives/youtube.js
+++ app/scripts/directives/youtube.js
@@ -2,14 +2,23 @@
 
 angular.module('StickItOnApp')
 .directive('youtube', function ($sce) {
+  function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[7].length==11){
+      return match[7];
+    }else{
+      console("Url incorrect");
+    }
+  }
   return {
     restrict: 'EA',
     scope: { code:'=' },
     replace: true,
     template: '<div style="height:200px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen> </iframe></div>',
     link: function (scope) {
-      var youtubeID = scope.code.match(/youtube\.com.*?v[\/=](\w+)/)[1];
+      var youtubeID = youtube_parser(scope.code);
       scope.url = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + youtubeID);
-   }
- }
+    }
+  }
 });
